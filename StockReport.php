21d40df31b8bec9bc74712d8830f8f75f12176d3
MarsWorldd <92814291+MarsWorldd@users.noreply.github.com>
2024-12-02 13:12:37 -0500

    Add files via upload

--- StockReport.php
+++ StockReport.php
@@ -7,70 +7,64 @@
 <body>
 <h2>Stock Rankings<span class="developer">Lamar Burns</span></h2>
 
-<form>
- <label>ranking: </label> <input class="number" type="number" name="ranking" value="15" 
- <label>market cap in billions</label>
-  <select name="marketCapComparison">
-  <option value="gt">greater than</option>
-  <option value="lt">less than</option>
-</select>
- &nbsp;
-  <input class="number" type="number" step="0.01" name="marketCap" value="0" /> 
-  <input type="submit" value="query" />
-  </form>
- <br />
+<<form>
+<label>ranking: </label> <input class="number" type="number" name="ranking" value="15" /> 
+<input type="submit" value="query" />
+</form> 
+<br />
+<p> <ol>
+<li>highlight (green) companies with a market cap greater or equal to 40 billion dollars </li>
+ <li>highlight (red) companies with a market cap less than or equal to .15 billion dollars </li>
+ <p>
+  <table>
+<thead>
+  <tr>
+<th>rank</th>
+<th>symbol</th>
+<th>company</th>
+<th>quant</th>
+<th>sa authors</th>
+<th>wall street</th>
+<th>market cap in billions</th>
+</tr>
+</thead>
+<tbody>
+ <?php 
+include( 'function.php');
+$file = fopen("./data/TopStocks.csv","r"); 
+$count = 0;
 
+$row = fgetcsv(stream: $file);
+
+while(! feof(stream: $file));
+{
+$row = fgetcsv(stream: $file);
+$rank = $row[0];
+$symbol = $row[1];
+$companyName = $row[2];
+$quant = $row[3];
+$saAuthors = $row[4];
+$wallStreet = $row[5];
+$marketCap= $row[6];
+?>
+<tr class="">
+<td><?= $rank ?></td>
+<td><?= $symbol ?></td>
+<td><?= $companyName ?></td>
+<td class="number"><?= qpa($quant) ?></td>
+<td class="number"><?= qpa($saAuthors) ?></td>
+<td class="number"><?= qpa($wallStreet) ?></td>
+<td class="number"><?= money_in_billions ($marketCap) ?></td> 
+</tr>
 
 <?php
- $file = fopen("./data/TopStocks.csv","r"); 
- print_r(fgetcsv($file)) . "<hr />"; 
- print_r(fgetcsv($file)) . "<hr />"; 
- print_r(fgetcsv($file)) . "<br />"; 
- fclose($file);
- ?>
-
- <table>
- <tr>
- <th>rank</th>
- <th>symbol</th>
- <th>company</th> <th>quant</th>
- <th>sa authors</th>
- <th>wall street</th>
- <th>Market Cap In Billions</th>
- </tr>
-
-<tbody>
-     
- <?php
- $file = fopen("./data/TopStocksSmall.csv","r");
- while(! feof($file)) {
-  $row = fgetcsv($file);
-  $rank = $row[0];
-  $symbol = $row[1];
-  $companyName =  $row[2];
-  $quant = $row[3];
-  $saAuthors = $row[4];
-  $wallStreet = $row[5]; $marketCap= $row[6];
-  ?>
-  <tr class="">
-   <td><?= $rank ?></td>
-   <td><?= $symbol ?></td>
-   <td><?= $companyName ?></td>
-   <td class="number"><?= $quant ?></td>
-   <td class="number"><?= $saAuthors ?></td> 
-   <td class="number"><?= $wallStreet ?></td> 
-   <td class="number"><?= $marketCap ?></td> 
-  </tr>
- 
-  <?php
-  count++;
-  if($count >= 15){
-    break;
-  }
+ $count++;
+ if ($count > 30){
+ return;
+}
  }
-  fclose($file);
-  ?>
-</tbody>
-</table>
-</body>
-</html>
+fclose($file); ?>
+<tbody>
+<table>
+<body>
+<tml>
